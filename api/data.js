b880a500c0de889c7d23a93223a6d437676e0575
Suman Malik <sumanmalik2907@gmail.com>
2023-10-06 15:01:53 +0530

    closing connection

--- api/data.js
+++ api/data.js
@@ -1,5 +1,8 @@
 module.exports = (req, res) => {
+    // Set the "Connection" header to "close"
+    res.setHeader('Connection', 'close');
+  
+    // Send the response
     res.status(200).json({ message: 'Hello, World! This is from Data.js File' });
-    res.end();
   };
   
