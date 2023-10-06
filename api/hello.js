b30ee97a1483bdff9ac07bba3d1b2b373773f935
Suman Malik <sumanmalik2907@gmail.com>
2023-10-06 14:23:45 +0530

    Adding home page on vercel

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello, World!' });
+    res.status(200).json({ message: 'Hello, World! That is from hello.js file' });
   };
   
