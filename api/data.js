50a5650fe006ef3f5354f419624ef39aabd2135c
Suman Malik <sumanmalik2907@gmail.com>
2023-10-06 15:02:59 +0530

    back to previous version

--- api/data.js
+++ api/data.js
@@ -1,8 +1,4 @@
 module.exports = (req, res) => {
-    // Set the "Connection" header to "close"
-    res.setHeader('Connection', 'close');
-  
-    // Send the response
     res.status(200).json({ message: 'Hello, World! This is from Data.js File' });
   };
   
